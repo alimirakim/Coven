387e298752c76a8cb82439f18863f60e577d7969
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2021-01-30 00:18:24 -0500

    Saved forms properly

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,4 +1,6 @@
 
+// import { deleteCookies } from '../../utils.js';
+import { loggedIn } from './utils.js';
 const query = document.querySelector.bind(document)
 const queryAll = document.querySelectorAll.bind(document)
 const create = document.createElement.bind(document)
@@ -21,6 +23,8 @@ const signupForm = query("#sup-form")
 const signupSmallBtn = query(".sup-sml")
 const signupErrorsSection = query(".sup-err")
 
+// ELEMENT VARIABLES Logout Button
+
 // ELEMENT VARIABLES Form inputs
 const signupFirstNameInput = query(".sup-first")
 const signupLastNameInput = query(".sup-last")
@@ -30,7 +34,70 @@ const signupVerifyPasswordInput = query(".sup-verify")
 const loginEmailInput = query(".sup-email")
 const loginPasswordInput = query(".sup-password")
 
-console.log("\nREACHING?")
+
+// LISTENER FUNCTIONS Signup submit
+function signupNewUser() {
+  signupForm.addEventListener("submit", async (ev) => {
+    ev.preventDefault()
+    const form = new FormData(signupForm)
+    const firstName = form.get("firstName")
+    const lastName = form.get("lastName")
+    const email = form.get("email")
+    const password = form.get("password")
+    const confirmPassword = form.get("verifyPassword")
+
+    const res = await fetch(`/api/users/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, email, password, confirmPassword })
+    })
+
+    if (res.ok) {
+      generateWelcomeMessage(user)
+      await fadeoutElement(signupPage)
+      updateLoginStateElements()
+      location.reload()
+
+    } else {
+      const { title, errors } = await res.json()
+      displayErrors(title, errors, signupErrorsSection)
+    }
+  })
+}
+
+// HELPER signupNewUser()
+function generateWelcomeMessage(user) {
+  const h2Welcome = create("h2")
+  h2Welcome.innerHTML = "Thank you"
+  const buttonX = create("button")
+  buttonX.classList.add("log-x", "l-exit")
+  buttonX.setAttribute("type", "button")
+  buttonX.innerHTML = "X"
+  const p1 = create("p")
+  p1.innerHTML = `Thank you for joining us,${user.firstName} ${user.lastName}.`
+  const p2 = create("p")
+  p2.innerHTML = `Welcome to the sisterhood.`
+  signupPage.replaceChildren(h2Welcome, buttonX, p1, p2)
+}
+
+// HELPER general, style
+async function fadeoutElement(el) {
+  await new Promise(resolve => {
+    setTimeout(() => {
+      el.classList.add("is-fading")
+      resolve()
+    }, 1500)
+  })
+  await new Promise(resolve => {
+    setTimeout(() => {
+      el.classList.add("is-hidden")
+      el.classList.remove("is-fading")
+      resolve()
+    }, 500)
+  })
+}
+
+
 // LISTENER login submit
 function loginUser() {
   loginForm.addEventListener("submit", async (ev) => {
@@ -38,22 +105,50 @@ function loginUser() {
     const form = new FormData(loginForm)
     const email = form.get("email")
     const password = form.get("password")
+
+    // Sets cookies
     const res = await fetch(`/api/users/token`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
     })
     if (res.ok) {
-      const { token, id } = await res.json()
-      // localStorage.setItem("COVEN_TOKEN", token)
-      // localStorage.setItem("COVEN_ID", id)
       updateLoginStateElements()
       loginPage.classList.add("is-hidden")
+      window.location.reload()
     } else {
       const { title, errors } = await res.json()
       displayErrors(title, errors, loginErrorsSection)
     }
   })
+}
+
+
+// HELPER forms, element-creation
+function displayErrors(title, errors, errorSection) {
+  const h3 = create("h3")
+  h3.innerHTML = title
+  const ul = create("ul")
+  errors.forEach(error => {
+    ul.innerHTML += `<li>${error}</li>`
+  })
+  errorSection.replaceChildren(h3, ul)
+}
+
+
+// HELPER general, state-change
+function updateLoginStateElements() {
+  let isLoggedIn = loggedIn()
+  const anonElements = queryAll(".anon")
+  const userElements = queryAll(".user")
+
+  if (isLoggedIn) {
+    for (const el of anonElements) { el.classList.add('is-hidden') }
+    for (const el of userElements) { el.classList.remove('is-hidden') }
+  } else {
+    for (const el of anonElements) { el.classList.remove('is-hidden') }
+    for (const el of userElements) { el.classList.add('is-hidden') }
+  }
 }
 
 
@@ -67,16 +162,16 @@ function toggleHiddenStateOnClick(el, btn) {
 
 // HELPER forms, state-reset/state-swap
 function toggleSignupLoginForm(btn) {
+  console.log("click")
   if (btn.classList.contains("log-sml")) {
-
-    // for (const el of signupElements) { el.classList.add("is-hidden") }
-    // for (const el of loginElements) { el.classList.remove("is-hidden") }
+    for (const el of signupElements) { el.classList.add("is-hidden") }
+    for (const el of loginElements) { el.classList.remove("is-hidden") }
     loginEmailInput.value = "demo@user.com"
     loginPasswordInput.value = "1234567890"
     loginErrorsSection.innerHTML = ""
   } else {
-    // for (const el of signupElements) { el.classList.remove("is-hidden") }
-    // for (const el of loginElements) { el.classList.add("is-hidden") }
+    for (const el of signupElements) { el.classList.remove("is-hidden") }
+    for (const el of loginElements) { el.classList.add("is-hidden") }
     signupFirstNameInput.value = ""
     signupLastNameInput.value = ""
     signupEmailInput.value = ""
@@ -89,7 +184,6 @@ function toggleSignupLoginForm(btn) {
 
 // HELPER forms, consolidate show-form button clicks
 function toggleDisplayLoginSignupForms() {
-  console.log("\ntoggle~!")
   for (const btn of signupBtns) { toggleHiddenStateOnClick(signupPage, btn) }
   for (const btn of loginBtns) { toggleHiddenStateOnClick(loginPage, btn) }
   toggleHiddenStateOnClick(signupPage, signupExitBtn)
@@ -104,8 +198,36 @@ function swapLoginSignupForm() {
 }
 
 
+// HELPER Log user out by deleting cookies with passed expiry
+function deleteCookies() {
+  const rawCookies = document.cookie.split("; ")
+  rawCookies.forEach((cookie, i) => {
+    const localHostName = window.location.hostname.split(".")
+    while (localHostName.length > 0) {
+      const cookieBase = encodeURIComponent(cookie.split(";")[0].split("=")[0]) + "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=" + localHostName.join(".") + " ;path="
+      const path = location.pathname.split('/')
+      document.cookie = cookieBase + "/"
+      while (path.length > 0) {
+        document.cookie = cookieBase + path.join("/")
+        path.pop()
+      }
+      localHostName.shift()
+    }
+  })
+}
+
+// Log user out on button-click
+function logoutUser() {
+  const logoutBtn = query(".logout")
+  logoutBtn?.addEventListener("click", (e) => {
+    deleteCookies()
+    location.reload()
+  })
+}
+
 signupNewUser()
 loginUser()
+logoutUser()
 toggleDisplayLoginSignupForms()
 swapLoginSignupForm()
-updateLoginStateElements()
+// updateLoginStateElements()
